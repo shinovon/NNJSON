20cc9581dcd88d01d1e5a6309bf3a931adde28d4
Shinovon <arman200711@gmail.com>
2026-07-05 07:56:08 +0500

    Bump version

--- src/cc/nnproject/json/JSON.java
+++ src/cc/nnproject/json/JSON.java
@@ -31,7 +31,7 @@ import java.util.Vector;
  * Usage:<p><code>JSONObject obj = JSON.getObject(str);</code></p>
  * <b>When using proguard, add</b>: <p><code>-optimizations !code/simplification/object</code>
  * @author Shinovon
- * @version 2.5
+ * @version 2.5a
  */
 public final class JSON {
 
